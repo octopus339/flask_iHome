4efb7ea063b8152ba2d535ee3513c12e6962ab8f
leslie <963082115@qq.com>
2018-04-20 16:08:09 +0800

    用单例初始化REST_SDK定义发送短信验证码方法

--- iHome/until/sms.py
+++ iHome/until/sms.py
@@ -25,27 +25,49 @@ serverPort='8883';
 #REST版本号
 softVersion='2013-12-26';
 
+class CCP(object):
+    #封装一个单例，用于统一发送短信验证码
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls,'_instance'):
+            cls._instance = super(CCP,cls).__new__(cls, *args, **kwargs)
+           #初始化REST SDK
+            cls._instance.rest = REST(serverIP,serverPort,softVersion)
+            cls._instance.rest.setAccount(accountSid,accountToken)
+            cls._instance.rest.setAppId(appId)
+        return cls._instance
+    def send_sms_code(self,to, datas, tempId):
+        #发送短信验证码的方法
+        #result 会返回smsMessageSid  dateCreated   statusCode三个字典格式的数据
+        result = self.rest.sendTemplateSMS(to, datas, tempId)
+        #判断短信是否发送成功，如果发送成功statusCode：000000
+        if result.statusCode == '000000':
+            #这里返回的值给调用者判断短信是否发送成功
+            return 1
+        else:
+            return 0
+
+
   # 发送模板短信
   # @param to 手机号码
   # @param datas 内容数据 格式为数组 例如：{'12','34'}，如不需替换请填 ''
   # @param $tempId 模板Id
 
-def sendTemplateSMS(to,datas,tempId):
-
-    
-    #初始化REST SDK
-    rest = REST(serverIP,serverPort,softVersion)
-    rest.setAccount(accountSid,accountToken)
-    rest.setAppId(appId)
-    
-    result = rest.sendTemplateSMS(to,datas,tempId)
-    for k,v in result.iteritems(): 
-        
-        if k=='templateSMS' :
-                for k,s in v.iteritems(): 
-                    print '%s:%s' % (k, s)
-        else:
-            print '%s:%s' % (k, v)
-    
-   
-sendTemplateSMS('13360226175',['leslie','5'],1)
+# def sendTemplateSMS(to,datas,tempId):
+#
+#
+#     #初始化REST SDK
+#     rest = REST(serverIP,serverPort,softVersion)
+#     rest.setAccount(accountSid,accountToken)
+#     rest.setAppId(appId)
+#
+#     result = rest.sendTemplateSMS(to,datas,tempId)
+#     for k,v in result.iteritems():
+#
+#         if k=='templateSMS' :
+#                 for k,s in v.iteritems():
+#                     print '%s:%s' % (k, s)
+#         else:
+#             print '%s:%s' % (k, v)
+#
+#
+# sendTemplateSMS('13360226175',['leslie','5'],1)
