721b5d8bbdaec32628329c1a4b5b6c88abf52a19
leslie <963082115@qq.com>
2018-04-23 15:44:18 +0800

    发布房源接口前端逻辑，收集每个input标签的name和value的值

--- iHome/static/js/ihome/newhouse.js
+++ iHome/static/js/ihome/newhouse.js
@@ -24,11 +24,42 @@ $(document).ready(function(){
             alert(response.errmsg)
         }
 
-    })
+    });
 
 
 
     // TODO: 处理房屋基本信息提交的表单数据
+    $('#form-house-info').submit(function (event) {
+        event.preventDefault();
+        var params={};
+        // 收集$(this).serializeArray表单中的所有带有name的input标签数据
+        //.map生成字典对象obj，封装到数组中,然后遍历每个字典
+        //// obj == {name:'title',value:'1'}
+        $(this).serializeArray().map(function (obj) {
+            params[obj.name] = obj.value
+        });
+        console.log(params);
+
+        // $.ajax({
+        //     url:'',
+        //     type:'post',
+        //     data:JSON.stringify(params),
+        //     contentType:'application/json',
+        //     headers:{'X-CSRFToken':getCookie('csrf_token')},
+        //     success:function (response) {
+        //         if (response.errno == 0){
+        //
+        //         }else if (response.errno == '4101'){
+        //             location.href = 'login.html'
+        //         }
+        //         else {
+        //             alert(response.errmsg)
+        //         }
+        //
+        //     }
+        // })
+
+    })
 
     // TODO: 处理图片表单的数据
 
