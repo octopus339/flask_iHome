60094415893ac70b13623807127620dbfa270b24
leslie <963082115@qq.com>
2018-04-23 09:45:54 +0800

    实名认证前端逻辑实现

--- iHome/static/js/ihome/auth.js
+++ iHome/static/js/ihome/auth.js
@@ -17,5 +17,48 @@ $(document).ready(function(){
 
 
     // TODO: 管理实名信息表单的提交行为
+  $('#form-auth').submit(function (envent) {
+        envent.preventDefault();
+        var real_name = $('#real-name').val();
+        var id_card = $('#id-card').val();
+        if (!real_name) {
+            alert('请输入真实的名字')
+        }
+        if (!id_card) {
+            alert('请输入真实的身份证号码')
+        }
 
+        var params = {
+            'real_name': real_name,
+            'id_card': id_card
+
+
+        };
+            $.ajax({
+                url: '/api/1.0/users/auth',
+                type: 'post',
+                data: JSON.stringify(params),
+                contentType: 'application/json',
+                headers: {'X-CSRFToken': getCookie('csrf_token')},
+                success: function (response) {
+                    if (response.errno == 0) {
+                        //因为实名认证只有一次，所以一旦成功，要将input标签设置为不可交互
+                        //保存按钮隐藏
+                        $('#real-name').attr('disabled', true);
+                        $('#id-card').attr('disabled', true);
+                        $('.btn-success').hide()
+
+
+                    } else if (response.errno == '4101') {
+                        location.href = 'login.html'
+                    }
+                    else {
+                        alert(response.errmsg)
+                    }
+
+
+                }
+            })
+
+    })
 })
